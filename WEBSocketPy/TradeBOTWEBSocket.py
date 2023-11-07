a50c24b1a1291b67ff6942d0451e1ca426103311
KonstantinSyitnikov <constantin.sytnikov@yandex.ru>
2023-11-07 11:16:21 +0300

    NoWorkinTatIsWrong

--- WEBSocketPy/TradeBOTWEBSocket.py
+++ WEBSocketPy/TradeBOTWEBSocket.py
@@ -9,6 +9,7 @@ class SocketConn(websocket.WebSocketApp):
     def __init__(self,url,params=[]):
         super().__init__(url=url,on_open=self.on_open)
         self.params=params
+      
         self.on_message=lambda ws ,msg: self.message(msg)
         self.on_error = lambda ws,e:print('Error',e)
         self.on_close=lambda ws:print('Closing')
@@ -21,10 +22,13 @@ class SocketConn(websocket.WebSocketApp):
         def run(*args):
             tradeStr={"op":"subscribe","args":self.params}   
             ws.send(json.dumps(tradeStr)) 
-        
+            
         _thread.start_new_thread(run())
+        
+        
 
     def message(self,msg):
+        
         print(msg)
 #ДАННЫЕ СВЕЧЕЙ
 #threading.Thread(target=SocketConn,args=('wss://stream.bybit.com/v5/public/linear',['kline.1.ETHUSDT'])).start()
